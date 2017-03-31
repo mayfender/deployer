acc493706128fbe6e72cf5ee2e6497a72ff7476c
sarawuti <sarawut.inthong@allianz.com>
2017-03-31 13:04:08 +0700

    update

--- license/src/main/java/com/may/ple/license/MyPrivateKeyProvider.java
+++ license/src/main/java/com/may/ple/license/MyPrivateKeyProvider.java
@@ -2,18 +2,23 @@ package com.may.ple.license;
 
 import java.io.File;
 import java.io.FileInputStream;
-
-import org.apache.commons.io.IOUtils;
+import java.security.CodeSource;
 
 import net.nicholaswilliams.java.licensing.encryption.PrivateKeyDataProvider;
 import net.nicholaswilliams.java.licensing.exception.KeyNotFoundException;
+
+import org.apache.commons.io.IOUtils;
 
 public class MyPrivateKeyProvider implements PrivateKeyDataProvider {
 
 	@Override
 	public byte[] getEncryptedPrivateKeyData() throws KeyNotFoundException {
 		try {
-			return IOUtils.toByteArray(new FileInputStream(new File("D:\\workspace\\64-bit\\mars\\exam1\\web\\private.key")));			
+			CodeSource codeSource = MyPrivateKeyProvider.class.getProtectionDomain().getCodeSource();
+			File currentPath = new File(codeSource.getLocation().toURI().getPath());
+			String keyDir = currentPath.getParentFile().getParentFile().getPath();
+			
+			return IOUtils.toByteArray(new FileInputStream(new File(keyDir + File.separator + "private.key")));			
 		} catch (Exception e) {
 			return null;
 		}
