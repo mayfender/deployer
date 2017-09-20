df49a995bee5436508c45c5c2fd756eaf192ca26
mayfender <mayfender@mypc>
2017-09-20 07:21:21 +0700

    Initial

---
+++ client/src/main/java/com/may/ple/tunnel/client/App.java
@@ -0,0 +1,13 @@
+package com.may.ple.tunnel.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
