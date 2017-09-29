b78c3e2a5f2586b8e373245b698deab171de54af
mayfender <mayfender@mypc>
2017-09-30 05:32:23 +0700

    update

--- client/src/main/java/jtcpfwd/NoMoreSocketsException.java
+++
@@ -1,4 +0,0 @@
-package jtcpfwd;
-
-public class NoMoreSocketsException extends Exception {
-}
--- client/src/main/java/jtcpfwd/util/udp/UDPHandler.java
+++
@@ -1,8 +0,0 @@
-package jtcpfwd.util.udp;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-
-public interface UDPHandler {
-	public void handle(byte[] data, InetSocketAddress address) throws IOException;
-}
