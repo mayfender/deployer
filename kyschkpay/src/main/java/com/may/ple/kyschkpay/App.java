e4f5d584cd4481953b269216814d591eb5225585
sarawuti <sarawut.inthong@allianz.com>
2017-10-20 17:51:46 +0700

    update

--- kyschkpay/src/main/java/com/may/ple/kyschkpay/App.java
+++ kyschkpay/src/main/java/com/may/ple/kyschkpay/App.java
@@ -1,6 +1,14 @@
 package com.may.ple.kyschkpay;
 
-import org.jsoup.Jsoup;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 
 
 /**
@@ -11,24 +19,29 @@ public class App {
 	
 	public static void main(String[] args) {
 		try {
-			/*String url = "http://localhost:8081/backend/tools/img2txt";
-			String username = "sadmin";
-			String password = "123";*/
+			HttpClient httpClient = HttpClientBuilder.create().build();
+			HttpPost httpPost = new HttpPost("http://localhost:8081/backend/login");
+			httpPost.addHeader("content-type", "application/json; charset=utf8");
 			
-			/*String json = Jsoup
-					.connect(url)
-					.data("username", username)
-					.data("password", new String(Base64.encodeBase64(password.getBytes())))
-					.method(Method.POST)
-					.header("X-Auth-Token", "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE1MDg1MzM1MjcsInN1YiI6InNhZG1pbiIsImF1ZGllbmNlIjoid2ViIiwiYXV0aG9yaXRpZXMiOlt7ImF1dGhvcml0eSI6IlJPTEVfU1VQRVJBRE1JTiJ9XSwiY3JlYXRlZCI6MTUwODQ5MDMyNzY3M30.xydmpquHtP1maLt5M_1rkQ7SEuoDGUKkgDiBIHBPQZf9TeVNGhqF_nBvFDuub0i7bFdsQrPqDNHxfhaJo_NIow")
-					.header("X-Requested-With", "XMLHttpRequest")
-					.execute()
-					.body();
-			System.out.println(json);*/			
+			 StringEntity userEntity = new StringEntity("{\"username\":\"sadmin\",\"password\":\"MTIz\"}");
+			 httpPost.setEntity(userEntity);
 			
-			String url = "http://localhost:8081/backend/restAct/tools/img2txt";
-			Jsoup.connect(url).post();
+			 HttpResponse response = httpClient.execute(httpPost);
 			
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+				   + response.getStatusLine().getStatusCode());
+			}
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+		
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+		
+			httpClient.getConnectionManager().shutdown();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
