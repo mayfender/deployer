93711a1108906dd7ea78cf4669b8615f5bbf61fd
sarawuti <sarawut.inthong@allianz.com>
2017-11-21 17:50:41 +0700

    update

--- kyschkpay/src/main/java/com/may/ple/kyschkpay/CaptchaResolve.java
+++ kyschkpay/src/main/java/com/may/ple/kyschkpay/CaptchaResolve.java
@@ -1,24 +1,19 @@
 package com.may.ple.kyschkpay;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -38,7 +33,7 @@ public class CaptchaResolve {
 		try {			
 			String baseDir = "D:/python_captcha";
 			String tesseractPath = "C:/Program Files (x86)/Tesseract-OCR";
-			String pythonPath = "C:\\Users\\mayfender\\AppData\\Local\\Programs\\Python\\Python36-32";
+			String pythonPath = "C:\\Users\\sarawuti\\AppData\\Local\\Programs\\Python\\Python36-32";
 			
 			return CaptchaUtil.tesseract(imgBase64, baseDir, tesseractPath, pythonPath);
 		} catch (Exception e) {
@@ -85,12 +80,28 @@ public class CaptchaResolve {
 			httpClient = builder.build();
 			HttpPost httpPost = new HttpPost("http://api.captchatronix.com/");
 			
-			List<NameValuePair> postParameters = new ArrayList<>();
+			/*List<NameValuePair> postParameters = new ArrayList<>();
 		    postParameters.add(new BasicNameValuePair("username", "mayfender"));
 		    postParameters.add(new BasicNameValuePair("password", "O4TIorHKqB"));
-		    postParameters.add(new BasicNameValuePair("function", "balance"));
+		    postParameters.add(new BasicNameValuePair("function", "picture2"));
+		    postParameters.add(new BasicNameValuePair("pic", "https://www.e-studentloan.ktb.co.th/STUDENT/Captcha.jpg"));*/
+			
+			
+			MultipartEntityBuilder multipart = MultipartEntityBuilder.create();
+			multipart.addTextBody("username", "mayfender", ContentType.TEXT_PLAIN);
+			multipart.addTextBody("password", "O4TIorHKqB", ContentType.TEXT_PLAIN);
+			multipart.addTextBody("function", "picture2", ContentType.TEXT_PLAIN);
+			File file = new File("D:\\python_captcha\\Captcha.jpg");
+			
+			/*HttpEntity data = MultipartEntityBuilder.create()
+                    .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
+                    .addTextBody("username", "mayfender", ContentType.DEFAULT_BINARY)
+                    .addTextBody("password", "O4TIorHKqB", ContentType.DEFAULT_BINARY)
+                    .addTextBody("function", "picture2", ContentType.DEFAULT_BINARY)
+                    .addTextBody("pic", "data:image/jpg;base64," + imgBase64, ContentType.DEFAULT_BINARY)
+                    .build();*/
 		    
-			httpPost.setEntity(new UrlEncodedFormEntity(postParameters, "UTF-8"));			
+			httpPost.setEntity(data);			
 			HttpResponse response = httpClient.execute(httpPost);
 	        
 			return entityStr(response);
@@ -104,18 +115,20 @@ public class CaptchaResolve {
 		try {
 			System.out.println(String.format("%1$tH:%1$tM:%1$tS", Calendar.getInstance().getTime()));
 			
-			/*File file = new File("D:\\python_captcha\\download.jpg");
+			/*File file = new File("D:\\python_captcha\\Captcha.jpg");
 			String base64String = Base64.encodeBase64String(FileUtils.readFileToByteArray(file));
 			String string = tesseract(base64String);
 			System.out.println(string);*/
 			
-			File file = new File("D:\\python_captcha\\download.jpg");
+			/*File file = new File("D:\\python_captcha\\download.jpg");
 			String base64String = Base64.encodeBase64String(FileUtils.readFileToByteArray(file));
 			String result = CaptchaResolve.captchasolutions(base64String);
-			System.out.println(result);
+			System.out.println(result);*/
 			
-			/*String txt = captchatronix(null);
-			System.out.println(txt);*/
+			File file = new File("D:\\python_captcha\\Captcha.jpg");
+			String base64String = Base64.encodeBase64String(FileUtils.readFileToByteArray(file));
+			String txt = captchatronix(base64String);
+			System.out.println(txt);
 			
 			System.out.println(String.format("%1$tH:%1$tM:%1$tS", Calendar.getInstance().getTime()));
 		} catch (Exception e) {
