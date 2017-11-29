32210dbf6bec4444be50000a77994b066ec2162c
sarawuti <sarawut.inthong@allianz.com>
2017-11-29 12:22:16 +0700

    update

--- kyschkpay/src/main/java/com/may/ple/kyschkpay/App.java
+++ kyschkpay/src/main/java/com/may/ple/kyschkpay/App.java
@@ -1,9 +1,5 @@
 package com.may.ple.kyschkpay;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
@@ -30,8 +26,6 @@ public class App {
 			
 			List<String> prodIds = Arrays.asList(args[0].split(","));
 			LOG.info("prodIds : " + prodIds);
-
-			socketApi();
 			
 			LOG.info("Start ManageLoginWorkerThread");
 			loginWorker = new ManageLoginWorkerThread(prodIds);
@@ -48,35 +42,6 @@ public class App {
 	public static boolean checkWorkingHour() {
 		int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
 		return hourOfDay >= App.START_WORKING_HOUR && hourOfDay <= App.END_WORKING_HOUR;
-	}
-	
-	private static void socketApi() {
-		LOG.info("Start socket api");
-		new Thread() {
-			public void run() {
-				ServerSocket serverSock = null;
-				
-				try {
-					serverSock = new ServerSocket(9001);	
-					
-					while(true) {
-						Socket socket = serverSock.accept();
-						BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-						String read;
-						while((read = reader.readLine())  != null) {
-							if(read.equals("SHUTDOWN")) System.exit(0);
-						}
-						socket.close();
-					}
-				} catch (Exception e) {
-					LOG.error(e.toString());
-				} finally {
-					try {
-						if(serverSock != null) serverSock.close();						
-					} catch (Exception e2) {}
-				}
-			}
-		}.start();
 	}
 	
 }
