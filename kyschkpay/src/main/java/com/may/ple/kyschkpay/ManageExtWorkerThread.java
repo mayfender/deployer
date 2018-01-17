44a4c4a148a9562a6f340f06562344165c9c9cc1
sarawuti <sarawut.inthong@allianz.com>
2018-01-17 14:03:44 +0700

    update

--- kyschkpay/src/main/java/com/may/ple/kyschkpay/ManageExtWorkerThread.java
+++ kyschkpay/src/main/java/com/may/ple/kyschkpay/ManageExtWorkerThread.java
@@ -10,9 +10,9 @@ import org.apache.log4j.Logger;
 
 public class ManageExtWorkerThread extends Thread {
 	private static final Logger LOG = Logger.getLogger(ManageExtWorkerThread.class.getName());
-	private static final int CORE_POOL_SIZE = 25;
+	private static final int CORE_POOL_SIZE = 10;
 	private static final int MAXIMUM_POOL_SIZE = 50;
-	private static final int KEEP_ALIVE_TIME = 180;
+	private static final int KEEP_ALIVE_TIME = 15;
 	private static final int SERVER_PORT = 9001;
 	
 	@Override
@@ -21,7 +21,7 @@ public class ManageExtWorkerThread extends Thread {
 				ServerSocket serv = new ServerSocket(SERVER_PORT);
 			) {
 			
-			ThreadPoolExecutor executor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+			ThreadPoolExecutor executor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
 			Socket client;
 			
 			while(true) {
