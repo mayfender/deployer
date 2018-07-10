5a45872dfd7ebc2e9414ec86a7e99aabfe985b8f
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2018-07-10 23:00:25 +0700

    update

---
+++ jwebsocketcust/src/com/may/ple/jwebsocket/ClientSimple.java
@@ -0,0 +1,55 @@
+package com.may.ple.jwebsocket;
+
+import org.jwebsocket.api.WebSocketClientEvent;
+import org.jwebsocket.api.WebSocketClientTokenListener;
+import org.jwebsocket.api.WebSocketPacket;
+import org.jwebsocket.client.token.JWebSocketTokenClient;
+import org.jwebsocket.token.Token;
+
+public class ClientSimple implements WebSocketClientTokenListener {
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println("Start");
+			
+			JWebSocketTokenClient client2 = new JWebSocketTokenClient();
+			client2.addTokenClientListener(new ClientSimple());
+			
+			client2.open("ws://localhost:8787/jWebSocket/jWebSocket");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Finish");
+	}
+
+	@Override
+	public void processOpening(WebSocketClientEvent aEvent) {
+		System.out.println("processOpening");
+	}
+
+	@Override
+	public void processOpened(WebSocketClientEvent aEvent) {
+		System.out.println("processOpened");
+	}
+
+	@Override
+	public void processPacket(WebSocketClientEvent aEvent, WebSocketPacket aPacket) {
+		System.out.println("processPacket");
+	}
+
+	@Override
+	public void processClosed(WebSocketClientEvent aEvent) {
+		System.out.println("processClosed");
+	}
+
+	@Override
+	public void processReconnecting(WebSocketClientEvent aEvent) {
+		System.out.println("processReconnecting");
+	}
+
+	@Override
+	public void processToken(WebSocketClientEvent aEvent, Token aToken) {
+		System.out.println("processToken");
+	}
+
+}
