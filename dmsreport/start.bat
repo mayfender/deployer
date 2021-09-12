576072cfa4b8198b35bcb6fe4503ce037bc0ebc5
mayfender <mayfender@gmail.com>
2021-09-12 13:01:55 +0700

    Delete dmsreport directory

--- dmsreport/start.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-set JAVA_OPTS=-Dfile.encoding=UTF-8 -Xms1G -Xmx2G -Duser.language=en -Duser.region=UK
-Rem copy javaw to javaw-DMS_Report for easy to determine
-start "" "C:\Program Files\Java\jdk1.8.0_151\bin\javaw-DMS_Report" %JAVA_OPTS% -jar dmsreport.jar
